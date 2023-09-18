7ea91ace2c1e1814fb5bfbf49821c8d10c530315
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-09-18 20:30:34 +0530

    patch n delete added

--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -6,4 +6,8 @@ router.get('/',todosController.getAllTodos)
 
 router.post('/',todosController.addTodo);
 
+router.patch('/:id',todosController.updateTodo);
+
+router.delete('/:id',todosController.deleteTodo);
+
 module.exports=router;
